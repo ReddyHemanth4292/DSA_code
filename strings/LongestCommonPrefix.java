1fdafc172bd4954d6a00e41b72f518c35aa7d0ef
ReddyHemanth4292 <rq0665@srmist.edu.in>
2026-08-26 23:28:32 +0530

    updated the LongestCommonPrefix question

--- strings/LongestCommonPrefix.java
+++ strings/LongestCommonPrefix.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 public class LongestCommonPrefix {
     public static String longestCommonPrefix(String[] strs){
         if(strs==null || strs.length==0){
-            return "Given array is empty";
+            return "";
         }
         String prefix=strs[0];
         for(int i=1;i< strs.length;i++){
-
             while(!strs[i].startsWith(prefix)){
                 prefix=prefix.substring(0,prefix.length()-1);
             }
             if(prefix.isEmpty()){
-                break;
+                return "";
             }
         }
         return prefix;
@@ -22,6 +21,10 @@ public class LongestCommonPrefix {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         String str=sc.nextLine();
+        if (str.trim().isEmpty()) {
+            System.out.println("");
+            return;
+        }
         String[] strArr=str.trim().split("\\s+");
         System.out.println(longestCommonPrefix(strArr));
     }
